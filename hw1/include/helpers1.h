4468b6fd4d00a2dc2a75103ea931d86843db2908
Natcha Jengjirapas <njengjir@uci.edu>
2021-06-22 13:47:11 -0700

    delete wrong folder

--- hw1/include/helpers1.h
+++
@@ -1,5 +0,0 @@
-// Declare all helper functions for hw1 in this file
-char checkFlags(char*);
-int checkPun(const char, const char*);
-int compare(int, int);
-int countSentences(const char, const char, const char*);
